dd0c21fd480b6f5cf88aec7bf29a44afeb9a9889
steve <standimitroff@icloud.com>
2020-01-28 16:34:05 +0200

    init example app

---
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Stanislav Dimitrov on 28.01.20.
+//  Copyright © 2020 Stanislav Dimitrov. All rights reserved.
+//
+
+import UIKit
+import FormKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
